625fba921aaf0660a104638328b156384ea4b7f8
Sahil Mansoori <sahilmansoori373@gmail.com>
2024-08-13 16:00:54 +0530

    check1

--- src/main/java/com/example/chatApp/controller/MessageController.java
+++ src/main/java/com/example/chatApp/controller/MessageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     @MessageMapping("/message")
+
     @SendTo("/to/return")
     public Message getContent(@RequestBody Message message){
         try {
