e0b07fe6a2a890f89661cbd48e1329dbcd938402
Sahil Mansoori <sahilmansoori373@gmail.com>
2024-08-14 14:10:57 +0530

    Fast communication

--- src/main/java/com/example/chatApp/controller/MessageController.java
+++ src/main/java/com/example/chatApp/controller/MessageController.java
@@ -14,13 +14,13 @@ public class MessageController {
 
     @SendTo("/to/return")
     public Message getContent(@RequestBody Message message){
-        try {
-            Thread.sleep(2000);
-
-        }
-        catch (InterruptedException e){
-            e.printStackTrace();
-        }
+//        try {
+//            Thread.sleep(2000);
+//
+//        }
+//        catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
         return message;
     }
 }
